627d0a4d7f451701eec92b05c0910702b35441fc
Астанин Иван <iastanin@softrust.ru>
2021-04-28 12:57:41 +0300

    Правка стилей

--- src/app/tasks/full-card/full-card.component.ts
+++ src/app/tasks/full-card/full-card.component.ts
@@ -3,7 +3,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
   selector: 'app-full-card',
   templateUrl: 'full-card.component.html',
-  styleUrls: ['full-card.component.scss', '../shared/card.style.scss'],
+  styleUrls: ['full-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FullCardComponent {}
--- src/app/tasks/short-card/short-card.component.ts
+++ src/app/tasks/short-card/short-card.component.ts
@@ -3,7 +3,7 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-short-card',
   templateUrl: './short-card.component.html',
-  styleUrls: ['./short-card.component.scss', '../shared/card.style.scss'],
+  styleUrls: ['./short-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShortCardComponent implements OnInit {
