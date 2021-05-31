8245314267e966fdba53664e7f7e7598b929562f
Астанин Иван <iastanin@softrust.ru>
2021-05-31 16:35:41 +0300

    loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {AppService} from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pet-ui';
+  constructor(public appService: AppService) {
+    this.appService.isLoading = true;
+    setTimeout(() => this.appService.isLoading = false, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CustomCheckboxModule} from './shared/components/custom-checkbox/custom-checkbox.module';
 import {MenuModule} from './menu/menu.module';
 import { FooterComponent } from './footer/footer.component';
+import {LoaderModule} from './shared/components/loader/loader.module';
+import {AppService} from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CustomCheckboxModule,
-    MenuModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CustomCheckboxModule,
+        MenuModule,
+        LoaderModule
+    ],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
